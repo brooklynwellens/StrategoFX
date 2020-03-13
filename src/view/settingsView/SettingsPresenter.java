a6df1b2503769d7c519f893ea3f701c9385c9789
brooklynwellens <58557901+brooklynwellens@users.noreply.github.com>
2020-03-13 17:46:23 +0100

    Add files via upload

--- src/view/settingsView/SettingsPresenter.java
+++ src/view/settingsView/SettingsPresenter.java
@@ -1,30 +1,28 @@
-package view.settingsView;
+package view.SettingsView;
 
+import com.sun.tools.javac.Main;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.media.AudioClip;
 import javafx.stage.Stage;
 import model.game.GameSetup;
 import view.mainMenu.MainMenuPresenter;
 import view.mainMenu.MainMenuView;
+import view.setupView.SetupView;
 
 public class SettingsPresenter {
-
+    private GameSetup model;
     private SettingsView view;
     private MainMenuView mainMenuView = new MainMenuView();
+    private boolean omkeren;
 
-
-    public SettingsPresenter(SettingsView view) {
+    public SettingsPresenter(GameSetup model, SettingsView view) {
+        this.model = model;
         this.view = view;
         addEventHandlers();
         updateView();
-    }
-
-    private void setScreenStandard() {
-        BorderPane.setMargin(view, new Insets(((double) (720 - 575) / 2), 0, 0, 160));
-        view.setScaleX(1);
-        view.setScaleY(1);
     }
 
     private void updateView() {
@@ -34,25 +32,31 @@ public class SettingsPresenter {
         view.getBtnFullscreen().setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
-
                 Stage stage = (Stage) view.getScene().getWindow();
-
-                if (!stage.isFullScreen()) {
-                    stage.setFullScreen(true);
-                    BorderPane.setMargin(view, new Insets(((double) (1280 - 720) / 2), 0, 0, 515));
-                    view.setScaleX(1.2);
-                    view.setScaleY(1.2);
-                } else if (stage.isFullScreen()){
-                    stage.setFullScreen(false);
-                    setScreenStandard();
-                }
+                stage.setFullScreen(true);
 
             }
         });
+
+        view.getBtnSound().setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if (!omkeren) {
+                    MainMenuView mainMenuView = new MainMenuView();
+                    mainMenuView.getMusic().play();
+                    mainMenuView.getMusic().setCycleCount(100);
+
+                }else{
+                    mainMenuView.getMusic().stop();
+                }
+                    omkeren = !omkeren;
+            }
+        });
+
         view.getBtnback().setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
-                MainMenuPresenter presenter = new MainMenuPresenter(mainMenuView);
+                MainMenuPresenter presenter = new MainMenuPresenter(model, mainMenuView);
                 view.getBtnback().getScene().setRoot(mainMenuView);
             }
         });
